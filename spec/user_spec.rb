8cdfeb6a50e6d3609eca03ac5a5b3e8662240e2f
obed300 <obamfo@st.ug.edu.gh>
2023-05-27 11:42:01 +0000

    Make changes

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -14,4 +14,17 @@ RSpec.describe User, type: :model do
     subject.posts_counter = 'a'
     expect(subject).to_not be_valid
   end
+
+  it 'brings last three posts for the user' do
+    user = User.new(name: 'Mert', posts_counter: 0)
+    user.save!
+
+    user.posts.create!(title: 'Post 1', text: 'First post', likes_counter: 5, comments_counter: 2)
+    user.posts.create!(title: 'Post 2', text: 'Second post', likes_counter: 5, comments_counter: 2)
+    post3 = user.posts.create!(title: 'Post 3', text: 'Third post', likes_counter: 5, comments_counter: 2)
+    post4 = user.posts.create!(title: 'Post 4', text: 'Fourth post', likes_counter: 5, comments_counter: 2)
+    post5 = user.posts.create!(title: 'Post 5', text: 'Fifth post', likes_counter: 5, comments_counter: 2)
+
+    expect(user.three_most_recent).to eq([post5, post4, post3])
+  end
 end
