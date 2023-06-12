2f60fa46cc365055f7ae20fdd12403c6b0f8701b
obed300 <obamfo@st.ug.edu.gh>
2023-06-12 17:02:03 +0000

    Update file

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,20 +1,43 @@
 require 'rails_helper'
 
-RSpec.describe 'Posts', type: :request do
-  describe 'GET all posts for the user' do
-    it 'checks whether it brings successful response' do
-      get user_posts_path(:user_id)
-      expect(response).to be_successful
-    end
+RSpec.describe 'GET posts/', type: :request do
+  let(:user) { User.create!(name: 'John Doe', photo: 'https://example.com/photo.jpg', bio: 'Hello, I am John Doe', posts_counter: 5) }
+  let(:post) { Post.create!(title: 'First Post', author_id: user.id, comments_counter: 3, likes_counter: 5) }
 
-    it 'renders the index template' do
-      get user_posts_path(:user_id)
-      expect(response).to render_template(:index)
-    end
+  it 'returns http status ok for /users/:id/posts' do
+    get user_posts_path(user.id)
+    expect(response.status).to eq(200)
+    expect(response.status).not_to eq(404)
+  end
 
-    it 'displays the body paragraph for users' do
-      get user_posts_path(:user_id)
-      expect(response.body).to include(' <h1>Route for list of post</h1>')
-    end
+  it 'renders the index template for posts controller' do
+    get user_posts_path(user.id)
+    expect(response).to render_template(:index)
+  end
+
+  it 'displays index content for posts controller' do
+    get user_posts_path(user.id)
+    expect(response.body).to include('Number of posts')
+  end
+end
+
+RSpec.describe 'GET /user/:id/posts/', type: :request do
+  let(:user) { User.create!(name: 'John Doe', photo: 'https://example.com/photo.jpg', bio: 'Hello, I am John Doe', posts_counter: 5) }
+  let(:post) { Post.create!(title: 'First Post', author_id: user.id, comments_counter: 3, likes_counter: 5) }
+
+  it ' returns http status ok for /user/:id/posts/:id' do
+    get user_posts_path(user.id)
+    expect(response.status).to eq(200)
+    expect(response.status).not_to eq(404)
+  end
+
+  it 'renders the show template' do
+    get user_post_path(user.id, post.id)
+    expect(response).to render_template(:show)
+  end
+
+  it 'displays show content for user post' do
+    get user_post_path(user.id, post.id)
+    expect(response.body).to include('Comments:')
   end
 end
