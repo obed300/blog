77667fd64ddee33408e96e86d0decf356283b075
obed300 <obamfo@st.ug.edu.gh>
2023-06-13 14:35:21 +0000

    Add devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-  before_action :current_user
+  # protect_from_forgery with: :exception
+  # before_action :current_user
 
-  def current_user
-    @current_user = User.first
-  end
+  # def current_user
+  #   @current_user = User.first
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, controllers: {
+    sessions: 'admin_users/sessions',
+    registrations: 'admin_users/registrations'
+  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
